b42271c09b9a5a128e94c6e3f0297d42e5012642
walid101 <nuhankhan101@gmail.com>
2021-06-20 14:36:46 -0400

    Fixed Landing page Logic

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('page', views.page, name="page"),
 ]
--- moviesite/views.py
+++ moviesite/views.py
@@ -38,9 +38,10 @@ print(" \n AND DESCS: \n")
 desc_list = getDescsFromMovies(test_list)
 print(desc_list)
 '''
-def home(request):
+async def home(request):
     #Begin Initial Search
     if(request.GET.get('movie_search')):
+         print(request)
          movie_list = [] #Movie List for Top carousel
          base_string = request.GET.get('movie_search')
          movie_list = get_movie(base_string, movie_list, 0, 2)
@@ -53,62 +54,15 @@ def home(request):
     
     #Landing search when user first lands on homepage
     base_string = "Superman"
-    movie_list = client.search_movie(base_string, tomatoes = False)
-    movie_descs = []
-    movie_sects = []
-    counter = 0
-    for movie in movie_list:
-               movie_descs.append(client.get(title=movie["title"], tomatoes=False))
-               curr_desc = movie_descs[counter]
-               movie_descs[counter]["plot_full"] = movie_descs[counter]["plot"]
-               if(len(curr_desc["plot"]) > char_lim):
-                    curr_desc["plot"] = (curr_desc["plot"])[0:char_lim-3] + " ..."
-                    movie_descs[counter] = curr_desc
-               counter+=1
-    if len(movie_descs) > 4*num_sections:
-         movie_sects = movie_descs[0:4*num_sections]
-    else:
-         movie_sects = movie_descs[0:len(movie_descs)]
-         limit_reached = False
-         while(len(movie_sects) < 4*num_sections and len(base_string) > 0 and limit_reached == False):
-              base_string = base_string[0:len(base_string)-1]
-              temp_movie_list = client.search_movie(base_string)
-              for movie in temp_movie_list:
-                   if(len(movie_sects) >= 4*num_sections):
-                        limit_reached = True
-                        break
-                   else:
-                        temp_sect = client.get(title = movie["title"], tomatoes=False)
-                        if temp_sect["poster"] != "NA":
-                             movie_sects.append(temp_sect)
+    movie_list = [] #Movie List for Top carousel
+    movie_list = get_movie(base_string, movie_list, 0, 2)
+    #movie_main = pageValPair(movie_list, 5)
+    movie_descs = getDescsFromMovies(movie_list)
+    movie_sects = movie_descs.copy()
     return render(request, 'home.html', context={"desc": movie_descs, "desc_rem":movie_descs[1:], "movie_sect1": movie_sects[0:4],
                                                  "movie_sect2": movie_sects[4:8], "movie_sect3":movie_sects[8:12],"movie_rem": movie_sects[len(movie_descs):],"deep_movies": movie_sects},) #{} is context dictionary
 
-'''
-def filter_no_poster(movie):
-     if len(movie["poster"]) == 0:
-          return False
-     return True
-
-def in_archive(archive, id, runtime):
-     if(archive.get(id) != None):
-          return True
-     else:
-          archive[id] = runtime
-          return False
-
-def deep_search(base_string, start_movies, archive):
-     new_keyword = "The "
-     deep_movie_list = client.search_movie(new_keyword+base_string, tomatoes = False)
-     deep_movie_desc = start_movies.copy()
-     for movie in deep_movie_list:
-          if(in_archive(archive, movie["imdb_id"], "100") == False):
-               temp_sect = client.get(title = movie["title"], tomatoes=True)
-               if(temp_sect["poster"] != "N/A"):
-                    if(len(temp_sect["plot"]) > char_lim):
-                         temp_sect["plot"] = (temp_sect["plot"])[0:char_lim-3] + " ..."
-                    deep_movie_desc.append(temp_sect)
-     return deep_movie_desc
-'''
+def page(request):
+     print("new page requested!")
 
      
